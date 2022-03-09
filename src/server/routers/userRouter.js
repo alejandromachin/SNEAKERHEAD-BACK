0fdd9a8fd3b3cbb77e3055d6a9dcd023ba1a3f41
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-09 11:54:09 +0100

    imported registerUser to userRouter

--- src/server/routers/userRouter.js
+++ src/server/routers/userRouter.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
 const express = require("express");
+const {
+  registerUser,
+} = require("../controllers/userController/userController");
 
 const router = express.Router();
 
