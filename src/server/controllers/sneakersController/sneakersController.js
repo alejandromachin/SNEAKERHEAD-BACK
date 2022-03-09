eb5ad0814d0973664b6e3205c6d6f70186d5a457
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-09 13:22:26 +0100

    added getAllsneakerByBrand middleware with error catch

--- src/server/controllers/sneakersController/sneakersController.js
+++ src/server/controllers/sneakersController/sneakersController.js
@@ -2,7 +2,14 @@ const Sneaker = require("../../../database/models/Sneaker");
 
 const getAllSneakersByBrand = async (req, res, next) => {
   const { brand } = req.params;
-  const allSneakersByBrand = await Sneaker.find({ brand });
+  try {
+    const allSneakersByBrand = await Sneaker.find({ brand });
+    res.json(allSneakersByBrand);
+  } catch {
+    const error = new Error("We could not find any sneaker by that brand");
+    error.code = 404;
+    next(error);
+  }
 };
 
 module.exports = { getAllSneakersByBrand };
