44f72cc64b7490049b9a7f5705ceebab3950d32b
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-15 12:19:32 +0100

    added iamge uploading

--- src/server/controllers/adsController/adsController.js
+++ src/server/controllers/adsController/adsController.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
+const { ref, uploadBytes, getDownloadURL } = require("firebase/storage");
 const fs = require("fs");
 const path = require("path");
 
 const Ad = require("../../../database/models/Ad");
 const Sneaker = require("../../../database/models/Sneaker");
+const storage = require("../../utils/firebaseConfig");
 
 const loadSneakerAds = async (req, res, next) => {
   const { id } = req.params;
@@ -36,6 +38,7 @@ const deleteAd = async (req, res, next) => {
     next(error);
   }
 };
+
 const loadSneakerAdInfo = async (req, res, next) => {
   const { id } = req.params;
 
@@ -49,8 +52,10 @@ const loadSneakerAdInfo = async (req, res, next) => {
     res.json(adInfo);
   }
 };
+
 const createAd = async (req, res, next) => {
   const data = req.body;
+  const images = [];
   try {
     const newAd = await Ad.create(data);
 
@@ -72,44 +77,146 @@ const createAd = async (req, res, next) => {
       "uploads",
       req.files.image2[0].originalname
     );
-    const oldFilenameImage3 = path.join(
-      "uploads",
-      req.files.image3[0].filename
-    );
-    const newFileNameImage3 = path.join(
-      "uploads",
-      req.files.image3[0].originalname
-    );
+    // const oldFilenameImage3 = path.join(
+    //   "uploads",
+    //   req.files.image3[0].filename
+    // );
+    // const newFileNameImage3 = path.join(
+    //   "uploads",
+    //   req.files.image3[0].originalname
+    // );
+    // const oldFilenameImage4 = path.join(
+    //   "uploads",
+    //   req.files.image4[0].filename
+    // );
+    // const newFileNameImage4 = path.join(
+    //   "uploads",
+    //   req.files.image4[0].originalname
+    // );
+    const url1 = async () => {
+      const image1Url3level = await fs.rename(
+        oldFilenameImage1,
+        newFileNameImage1,
+        async () => {
+          const image1Url2level = await fs.readFile(
+            newFileNameImage1,
+            async (error, file) => {
+              if (error) {
+                next(error);
+              } else {
+                const fileRef = ref(storage, newFileNameImage1);
+                await uploadBytes(fileRef, file);
 
-    const oldFilenameImage4 = path.join(
-      "uploads",
-      req.files.image4[0].filename
-    );
-    const newFileNameImage4 = path.join(
-      "uploads",
-      req.files.image4[0].originalname
-    );
+                const image1Url = await getDownloadURL(fileRef);
+                return image1Url;
+                // const ad = await Ad.findById(newAd.id);
 
-    fs.rename(oldFilenameImage1, newFileNameImage1, (error) => {
-      if (error) {
-        next(error);
-      }
-    });
-    fs.rename(oldFilenameImage2, newFileNameImage2, (error) => {
-      if (error) {
-        next(error);
-      }
-    });
-    fs.rename(oldFilenameImage3, newFileNameImage3, (error) => {
-      if (error) {
-        next(error);
-      }
-    });
-    fs.rename(oldFilenameImage4, newFileNameImage4, (error) => {
-      if (error) {
-        next(error);
-      }
-    });
+                // ad.images.push(image1Url);
+
+                // await Ad.findByIdAndUpdate(newAd.id, {
+                //   images: ad.images,
+                // });
+              }
+              return image1Url2level;
+            }
+          );
+        }
+      );
+      return image1Url3level;
+    };
+
+    const url2 = async () => {
+      const image2Url3level = await fs.rename(
+        oldFilenameImage2,
+        newFileNameImage2,
+        async () => {
+          const image2Url2level = await fs.readFile(
+            newFileNameImage2,
+            async (error, file) => {
+              if (error) {
+                next(error);
+              } else {
+                const fileRef = ref(storage, newFileNameImage2);
+                await uploadBytes(fileRef, file);
+
+                const image2Url = await getDownloadURL(fileRef);
+                return image2Url;
+              }
+              return image2Url2level;
+            }
+          );
+        }
+      );
+      return image2Url3level;
+    };
+
+    (async () => {
+      await url1();
+      await url2();
+      images.push(url1);
+      images.push(url2);
+      await Ad.findByIdAndUpdate(newAd.id, {
+        images,
+      });
+    })();
+
+    // await fs.rename(oldFilenameImage2, newFileNameImage2, async () => {
+    //   await fs.readFile(newFileNameImage2, async (error, file) => {
+    //     if (error) {
+    //       next(error);
+    //     }
+    //     const fileRef = ref(storage, newFileNameImage2);
+    //     await uploadBytes(fileRef, file);
+
+    //     const image2Url = await getDownloadURL(fileRef);
+
+    //     const ad = await Ad.findById(newAd.id);
+
+    //     ad.images.push(image2Url);
+
+    //     await Ad.findByIdAndUpdate(newAd.id, {
+    //       images: ad.images,
+    //     });
+    //   });
+    // });
+
+    // await fs.rename(oldFilenameImage3, newFileNameImage3, async () => {
+    //   await fs.readFile(newFileNameImage3, async (error, file) => {
+    //     if (error) {
+    //       next(error);
+    //     }
+    //     const fileRef = ref(storage, newFileNameImage3);
+    //     await uploadBytes(fileRef, file);
+
+    //     const image3Url = await getDownloadURL(fileRef);
+
+    //     const ad = await Ad.findById(newAd.id);
+
+    //     ad.images.push(image3Url);
+
+    //     await Ad.findByIdAndUpdate(newAd.id, {
+    //       images: ad.images,
+    //     });
+    //   });
+    // });
+    // await fs.rename(oldFilenameImage4, newFileNameImage4, async () => {
+    //   await fs.readFile(newFileNameImage4, async (error, file) => {
+    //     if (error) {
+    //       next(error);
+    //     }
+    //     const fileRef = ref(storage, newFileNameImage4);
+    //     await uploadBytes(fileRef, file);
+
+    //     const image4Url = await getDownloadURL(fileRef);
+
+    //     const ad = await Ad.findById(newAd.id);
+    //     ad.images.push(image4Url);
+
+    //     await Ad.findByIdAndUpdate(newAd.id, {
+    //       images: ad.images,
+    //     });
+    //   });
+    // });
 
     res.json(newAd);
   } catch {
