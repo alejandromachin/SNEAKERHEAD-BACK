9650659837901a34b84de232b8da7d7f3598d9bc
Alejandro Rodríguez <a.rodriguezmachin@gmail.com>
2022-03-16 16:30:50 +0100

    added tests on login endpoint

--- src/server/controllers/userController/userController.test.js
+++ src/server/controllers/userController/userController.test.js
@@ -1,6 +1,8 @@
+const bcrypt = require("bcrypt");
+const jsonwebtoken = require("jsonwebtoken");
 const User = require("../../../database/models/User");
 
-const { registerUser } = require("./userController");
+const { registerUser, loginUser } = require("./userController");
 
 jest.mock("../../../database/models/User");
 
@@ -37,6 +39,54 @@ describe("Given a registerUser middleware", () => {
       await registerUser(req, res, null);
 
       expect(res.json).toHaveBeenCalledWith(userCreated);
+    });
+  });
+});
+describe("Given a getLogin function", () => {
+  describe("When it receives a response", () => {
+    test("Then if the user does not exist it should throw an error with the status code 404 and the error message 'Username or password are wrong'", async () => {
+      const req = {
+        body: { username: "machinazo", password: "1234" },
+      };
+      const next = jest.fn();
+      const error = new Error("Username or password are wrong");
+      User.find = jest.fn().mockRejectedValue(null);
+
+      await loginUser(req, null, next);
+
+      expect(next).toHaveBeenCalledWith(error);
+    });
+
+    test("Then if the user exists but the password is not correct", async () => {
+      const req = {
+        body: { username: "machinazo", password: "1234" },
+      };
+      const next = jest.fn();
+      const error = new Error("Username or password are wrong");
+      User.find = jest.fn().mockResolvedValue(req.body);
+      bcrypt.compare = jest.fn().mockRejectedValue(false);
+
+      await loginUser(req, null, next);
+
+      expect(next).toBeCalledWith(error);
+    });
+
+    test("Then if the user exists and the password is right it should call the json method with the token", async () => {
+      const req = {
+        body: { username: "machinazo", password: "1234" },
+      };
+      const res = {
+        json: jest.fn(),
+      };
+      const token = "this is a token";
+
+      User.findOne = jest.fn().mockResolvedValue(req.body);
+      bcrypt.compare = jest.fn().mockResolvedValue(true);
+      jsonwebtoken.sign = jest.fn().mockReturnValue(token);
+
+      await loginUser(req, res, null);
+
+      expect(res.json).toHaveBeenCalledWith({ token });
     });
   });
 });
